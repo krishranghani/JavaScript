60e49554adbd583ab3bb97a3170e1cbd1915b189
krish ranghani <krishranghani@gmail.com>
2023-12-16 09:58:28 +0530

    16-12-2023

--- programs.js
+++ programs.js
@@ -3,14 +3,20 @@
 // revers.sort;
 // console.log(revers.reverse());
 
-// 2.
+// 2. Write a JS function that returns a passed string with letters in alphabetical order. 
+// Example string : 'webmaster'
+// Expected Output : 'abeemrstw'
+// Assume punctuation and numbers symbols are not included in the passed string.
+
 
 // function arr(str){
 //     return str . split('').sort().join('');
 // }
 // console.log(arr("webmaster"));
 
-// 3.
+// 3. Write a JS function that accepts a string as a parameter and converts the first letter of each word of the string in upper case. 
+// Example string : 'the quick brown fox'
+// Expected Output : 'The Quick Brown Fox'
 
 // const text1 = 'the quick brown fox';
 // const text2 = text1.toUpperCase();
@@ -31,7 +37,9 @@
   
   
 
-// 4.
+// 4. Write a JavaScript function that accepts a string as a parameter and finds the longest word within the string.
+// Example string : 'Web Development Tutorial'
+// Expected Output : 'Development'
   
 // function LongestWord(str) {
 //     let words = str.split(' ');
@@ -50,19 +58,23 @@
 //   let longest = LongestWord(inputString);
 //   console.log(longest);  // Output: 'Development'  
 
-// 5.
-// function isPerfectNumber(number) {
-//     let sum = 0;
-    
-//     for (let i = 1; i < number; i++) {
-//       if (number % i === 0) {
-//         sum += i;
-//       }
-//     }
-    
-//     return sum === number;
-//   }
-//   console.log(isPerfectNumber(6)); // Output: true
-//   console.log(isPerfectNumber(28)); // Output: true
-//   console.log(isPerfectNumber(12)); // Output: false
-    
+// 5. Write a JavaScript function that checks whether a number is perfect.
+
+function isPerfectNumber(number) {
+    let sum = 0;
+  
+    for (let i = 1; i < number; i++) {
+      if (number % i === 0) {
+        sum += i;
+      }
+    }
+  
+    if (sum === number) { 
+      console.log("This is perfect number");
+    } else {
+      console.log("This is not perfect number");
+    }
+  }
+  let number = Number(prompt("enter value"))
+  console.log("enterd value is ==>",number); 
+  isPerfectNumber(number); 
