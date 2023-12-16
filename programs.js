e427e9f075bade897115d4ab1e5de2e291560b50
krish ranghani <krishranghani@gmail.com>
2023-12-16 09:16:49 +0530

    15-12-2023

---
+++ programs.js
@@ -0,0 +1,68 @@
+// 1. reverse a number
+// const revers = [1,5,4,3,8];
+// revers.sort;
+// console.log(revers.reverse());
+
+// 2.
+
+// function arr(str){
+//     return str . split('').sort().join('');
+// }
+// console.log(arr("webmaster"));
+
+// 3.
+
+// const text1 = 'the quick brown fox';
+// const text2 = text1.toUpperCase();
+// console.log(text1);
+// console.log(text2);
+
+// function capitalFirstLetter(str) {
+//     let words = str.split(' ');
+//     for (let i = 0; i < words.length; i++) {
+//       words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+//     }
+//     let capitalizedStr = words.join(' ');
+//     return capitalizedStr;
+//   }
+//   let inputString = 'the quick brown fox';
+//   let outputString = capitalFirstLetter(inputString);
+//   console.log(outputString); // Output: 'The Quick Brown Fox'
+  
+  
+
+// 4.
+  
+// function LongestWord(str) {
+//     let words = str.split(' ');
+//     let longest = '';
+  
+//     for (var i = 0; i < words.length; i++) {
+//       if (words[i].length > longest.length) {
+//         longest = words[i];
+//       }
+//     }
+  
+//     return longest;
+//   }
+  
+//   let inputString = 'Web Development Tutorial';
+//   let longest = LongestWord(inputString);
+//   console.log(longest);  // Output: 'Development'  
+
+// 5.
+// function isPerfectNumber(number) {
+//     let sum = 0;
+    
+//     for (let i = 1; i < number; i++) {
+//       if (number % i === 0) {
+//         sum += i;
+//       }
+//     }
+    
+//     return sum === number;
+//   }
+//   console.log(isPerfectNumber(6)); // Output: true
+//   console.log(isPerfectNumber(28)); // Output: true
+//   console.log(isPerfectNumber(12)); // Output: false
+    
